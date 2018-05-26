4fa13e625d1789d3af5d75b5d150acfe4e46c96d
JohnZhang <471352728@qq.com>
2018-05-26 08:49:56 +0800

    first commit

---
+++ app/src/main/java/com/example/john/myframework/MyApplication.java
@@ -0,0 +1,10 @@
+package com.example.john.myframework;
+
+import android.app.Application;
+
+/**
+ * Created by yang on 2018/4/28.
+ */
+
+public class MyApplication extends Application {
+}
---
+++ app/src/main/java/com/example/john/myframework/SimpleIPCAty.java
@@ -0,0 +1,10 @@
+package com.example.john.myframework;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yang on 2018/5/23.
+ */
+
+public class SimpleIPCAty extends AppCompatActivity {
+}
