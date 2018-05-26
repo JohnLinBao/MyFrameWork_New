b519cb09069a8e539bd8743ecad701834cbde5cf
JohnZhang <471352728@qq.com>
2018-05-26 08:53:33 +0800

    第二次

--- app/src/main/java/com/example/john/myframework/MainActivity.java
+++ app/src/main/java/com/example/john/myframework/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.john.myframework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import easily.tech.guideview.lib.GuideViewBundle;
+import easily.tech.guideview.lib.GuideViewFragment;
+
+/**
+ * john
+ */
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        findViewById(R.id.guide).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 }
